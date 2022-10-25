5ac6b1d01f38e8cc49067b9779d4333d952fc1cf
catjeed <kadblp@yandex.ru>
2022-10-26 01:17:10 +0500

    minor fix

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const userRouter = require('./routes/users');
 const { PORT = 3000 } = process.env;
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://127.0.0.1/mestodb');
 
@@ -20,6 +21,7 @@ app.use((req, res, next) => {
 
 app.use('/users', userRouter);
 app.use('/cards', cardsRouter);
+app.use('/*', (req, res) => res.status(404).send({ message: 'Проверьте адрес запроса' }));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
