4db270646edb195b4d4ba403713f7b11facb76ce
catjeed <kadblp@yandex.ru>
2022-10-26 00:42:38 +0500

    some changes

--- controllers/users.js
+++ controllers/users.js
@@ -13,10 +13,9 @@ module.exports.createUser = (req, res) => {
     .then((user) => res.send(user))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(400).send({ message: 'Переданы некорректные данные в методы создания пользователя' });
-      } else {
-        res.status(500).send({ message: 'Произошла ошибка на сервере' });
+        return res.status(400).send({ message: 'Переданы некорректные данные в методы создания пользователя' });
       }
+      return res.status(500).send({ message: 'Произошла ошибка на сервере' });
     });
 };
 
@@ -24,20 +23,48 @@ module.exports.getUserById = (req, res) => {
   User.findById(req.params.userId)
     .then((user) => {
       if (!user) {
-        res.status(404).send({ message: `Пользователь с id ${req.params.userId} не найден` });
-      } else {
-        res.status(200).send({ data: user });
+        return res.status(404).send({ message: `Пользователь с id ${req.params.userId} не найден` });
       }
+      return res.status(200).send({ data: user });
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(400).send({ message: `Что-то пошло не так! ${err.name}: ${err.message}` });
-      } else {
-        res.status(500).send({ message: 'Произошла ошибка на сервере' });
+        return res.status(400).send({ message: 'Что-то пошло не так! Проверьте id' });
       }
+      return res.status(500).send({ message: 'Произошла ошибка на сервере' });
     });
 };
 
-module.exports.updateProfile = () => {};
+module.exports.updateProfile = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: `Пользователь с id ${req.user._id} не найден` });
+      }
+      return res.status(200).send(user, { message: 'Данные обновлены' });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные в методы создания пользователя' });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка на сервере' });
+    });
+};
 
-module.exports.updateAvatar = () => {};
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: `Пользователь с id ${req.user._id} не найден` });
+      }
+      return res.status(200).send(user, { message: 'Аватар обновлен' });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные при обновлении аватара' });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка на сервере' });
+    });
+};
